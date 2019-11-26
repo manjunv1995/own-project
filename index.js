d34e485717269d81e7dee75077461fea1ebacd5a
manjunath vijapuram <vmmanjunathece@gmail.com>
2019-11-26 17:47:26 +0530

    month added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function getTimeAndDate() {
     let onGoing = new Date();
-    let timeMinuts = onGoing.getMinutes();
     let timeHours = onGoing.getHours();
+    let timeMinuts = onGoing.getMinutes();
+    let amOrPm = timeHours <= 12 ? 'am' : 'pm';
+    let finalTime = timeHours = (timeHours % 12) || 12;
     let date = onGoing.getDate();
+    let month = onGoing.getMonth();
     let year = onGoing.getFullYear();
 
-    document.getElementById("demo").innerHTML = `${timeHours} :: ${timeMinuts} <br/> Date ==>> ${date} <br/> year ==>> ${year}`;
+    document.getElementById("demo").innerHTML = `${finalTime} :: ${timeMinuts}  ${amOrPm} <br/> date ==>> ${date} <br/> month ==> ${month+1} <br/> year ==>> ${year}`;
 }
+
+// HI
